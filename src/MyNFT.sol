13d3ab0ec5320019c3e7b070c88cdacdb7a63fba
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2025-05-29 19:03:26 +0000

    chore: Import IERC20 interface

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
  
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // Interface for PlatformCredits with permit
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol"; // Interface for PlatformCredits with permit
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol"; // For EIP-712 signature verification
